cbb9b121479690ffa63f5cdae251aaa43e6c2235
Saloni Fernandes <saloni.ferdz@gmail.com>
2022-05-06 15:26:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <header><h1>Counter app using State or Hooks</h1></header>
     <h2>Current value of count is {count}</h2>
     <button onClick={()=>setCount(count+1)}>Increase counter</button>
-    <button onClick={()=>count<0 ? "" :setCount(count-1)}>Decrease counter</button>
+    <button onClick={()=>count<=0 ? "" :setCount(count-1)}>Decrease counter</button>
     <button onClick={()=>setCount(0)}>Reset counter</button>
      </div>
   );
