71daee0c1ee543e6b9e518ea6a9985cfad25c668
Saloni_A_Fernandes <71886464+Saloni1416@users.noreply.github.com>
2022-05-06 12:22:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React,{useState} from "react";
+import "./App.css"
+
+function App() {
+  const [count,setCount]=useState(0);
+
+ 
+
+  return (
+    <div className="App">
+    <header><h1>Counter app using State or Hooks</h1></header>
+    <h2>Current value of count is {count}</h2>
+    <button onClick={()=>setCount(count+1)}>Increase counter</button>
+    <button onClick={()=>setCount(count-1)}>Decrease counter</button>
+    <button onClick={()=>setCount(0)}>Reset counter</button>
+     </div>
+  );
+}
+
+export default App;
